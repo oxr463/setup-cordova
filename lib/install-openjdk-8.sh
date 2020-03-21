42f71ae868662f1032a6f6b62da6b7cccf2bd8c7
Lucas Ramage <oxr463@gmx.us>
2020-03-20 20:06:10 -0400

    Delete install-openjdk-8.sh

--- lib/install-openjdk-8.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Install OpenJDK 8
-sudo apt-get update
-sudo apt-get install -y --no-install-recommends openjdk-8-jdk
-sudo apt-get clean
-sudo rm -rf /var/lib/apt/lists/*
-sudo rm -rf /var/cache/oracle-jdk8-installer
-java -version
